e17af661a1e1127f109da5a122e7c39e01bde810
Neelansh Mathur <neelansh15@outlook.com>
2020-03-14 19:28:49 +0530

    Removed API Keys and Phone numbers for Privacy

--- make_call.php
+++ make_call.php
@@ -1,25 +1,29 @@
+<title>Neelansh Calls</title>
 <?php
 require __DIR__ . '/vendor/autoload.php';
 use Twilio\Rest\Client;
 
 // Your Account SID and Auth Token from twilio.com/console
-$account_sid = 'AC63615603c8910ed9c583b88a992d9b7d';
-$auth_token = '8c21a0da5d983898df736f0b16b0bba0';
+$account_sid = '';
+$auth_token = '';
 // In production, these should be environment variables. E.g.:
 // $auth_token = $_ENV["TWILIO_ACCOUNT_SID"]
 
 // A Twilio number you own with Voice capabilities
-$twilio_number = "+12093967531";
+$twilio_number = "";
 
 // Where to make a voice call (your cell phone?)
-$to_number = "+918450950550";
+$to_number = "";
 
 $client = new Client($account_sid, $auth_token);
 $client->account->calls->create(  
     $to_number,
     $twilio_number,
     array(
-        "url" => "http://demo.twilio.com/docs/voice.xml"
+        // "url" => "http://demo.twilio.com/docs/voice.xml"
+        "url" => "https://neelansh.co/twilio/makecallvoice.xml"
     )
 );
+
+echo "<h1>Making the call from number <b>$twilio_number</b></h1>";
 
